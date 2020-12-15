f41f74dcfd54f2e54d2bfe32e70688f1f5572143
masaditya <aeka489@gmail.com>
2020-12-15 17:09:33 +0700

    exec series

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
-
+const series = require('async')
+const {exec} = require('child_process');
 try {
     const nameToGreet = core.getInput('who-to-greet')
     console.log(`Yo ngene! ${nameToGreet}`)
     const time = (new Date()).toTimeString()
     core.setOutput("time", time)
-
+    series.series([
+        () => exec('npm test 2>&1 | tee test.log'),
+        () => exec('cat test.log') 
+    ]); 
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`Event payload e ${payload}`)
 } catch (error) {
