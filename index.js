b72b59b46a56546b66df1c14863aa66a0c9a19d2
masaditya <aeka489@gmail.com>
2020-12-15 17:27:15 +0700

    promise

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const series = require('async')
 const {exec} = require('child_process');
+
+const runExec = async ()=> {
+    return await series.series([
+        () => exec('npm test 2>&1 | tee test.log'),
+        () => exec('cat test.log') 
+    ])
+}
+
 try {
     const nameToGreet = core.getInput('who-to-greet')
     console.log(nameToGreet)
     const time = (new Date()).toTimeString()
     core.setOutput("time", time)
-    const tmp = series.series([
-        () => exec('npm test 2>&1 | tee test.log'),
-        () => exec('cat test.log') 
-    ]); 
+    const tmp = await runExec()
     console.log(tmp)
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`Event payload e ${payload}`)
