c6b1ae46575abcaf18ac780dbe0554f1ab73b6ef
masaditya <aeka489@gmail.com>
2020-12-15 17:35:32 +0700

    save exec result

--- index.js
+++ index.js
@@ -4,21 +4,23 @@ const series = require('async')
 const {exec} = require('child_process');
 
 const runExec = async ()=> {
-    return await series.series([
+    return series.series([
         () => exec('npm test 2>&1 | tee test.log'),
         () => exec('cat test.log') 
     ])
 }
-
 try {
     const nameToGreet = core.getInput('who-to-greet')
-    console.log(nameToGreet)
+    // console.log(nameToGreet)
     const time = (new Date()).toTimeString()
     core.setOutput("time", time)
-    const tmp = await runExec()
-    console.log(tmp)
+    runExec().then(res => {
+        console.log(res)
+    }).catch(err => {
+        console.log(err)
+    })
     const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`Event payload e ${payload}`)
+    // console.log(`Event payload e ${payload}`)
 } catch (error) {
     core.setFailed(error.message)
 }
