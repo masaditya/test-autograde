743d646d6413d03a5e1d0908f93ab22fca0d340f
masaditya <aeka489@gmail.com>
2021-03-21 14:00:09 +0700

    grouping verbose detail

--- index.js
+++ index.js
@@ -6,25 +6,23 @@ const fs = require('fs');
 async function main(){
 try {
     const kelas = core.getInput('kelas');
-
     console.log(`Kelas ${kelas}!`);
-    
+    const week = core.getInput('week');
+    console.log(`Week ${week}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
-    
+    const {payload } = github.context.payload
     fs.readFile('test.log','utf-8', (err, data)=> {
       if(data){
-        // console.log(data)
-        parseData(data)
+        console.log( parseData(data))
       }
       else
         console.log("data empty")
     })
-
     sendData()
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
   } catch (error) {
     core.setFailed(error.message);
   }
@@ -34,17 +32,27 @@ main()
 
 function parseData (data=""){
   let arrString = data.split(/\s+/)
-  let indexFile = []
-  let countPass = arrString.filter((item, i) => {
-    if(item == "PASS"){
-      indexFile.push(i)
-      return item
+  let indexTestName = []
+  let indexTestNameClose = []
+  let indexFileName = []
+  let testResult = []
+
+
+  arrString.forEach((item, i) => {
+    if(item == "√" || item == "×"){
+      indexTestName.push(i)
+    }
+    if(item == "ms)"){
+      indexTestNameClose.push(i+1)
     }
   })
-  console.log(countPass)
-  indexFile.forEach(i => {
-    console.log(arrString[i+1])
-  })
+    
+  for (let i = 0; i < indexTestName.length; i++) {
+    let testName = arrString.slice(indexTestName[i], indexTestNameClose[i]).join(" ")
+    testResult.push(testName)
+  }
+
+  return testResult
 }
 
 async function sendData ( ){
