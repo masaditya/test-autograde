60f0093c0476220df075a3297a0492c8c19d3ffc
masaditya <aeka489@gmail.com>
2021-03-21 21:01:58 +0700

    create grade data

--- index.js
+++ index.js
@@ -7,22 +7,41 @@ async function main(){
 try {
     const kelas = core.getInput('kelas');
     console.log(`Kelas ${kelas}!`);
-    const week = core.getInput('week');
-    console.log(`Week ${week}!`);
+    const week = core.getInput('minggu-ke');
+    console.log(`Minggu ke-${week}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     const {payload } = github.context.payload
+    console.log(payload)
     fs.readFile('test.log','utf-8', (err, data)=> {
       if(data){
-        console.log( parseData(data))
+        const gradeData = {
+          user : {
+            name : payload.head_commit.author.name,
+            email : payload.head_commit.author.email,
+            username : payload.head_commit.author.masaditya
+          },
+          assignment : {
+            repo_url : payload.repository.html_url,
+            repo_name : payload.repository.full_name.split("/")[1],
+            week : week,
+            class : kelas
+          },
+          grade : {
+            correct : 0,
+            incorrect : 0,
+            detail : parseData(data)
+          }
+        }
+
+        console.log(gradeData)
+
       }
       else
         console.log("data empty")
     })
     sendData()
-    // Get the JSON webhook payload for the event that triggered the workflow
-    // const payload = JSON.stringify(github.context.payload, undefined, 2)
-    // console.log(`The event payload: ${payload}`);
+    
   } catch (error) {
     core.setFailed(error.message);
   }
@@ -34,17 +53,12 @@ function parseData (data=""){
   let arrString = data.split(/\s+/)
   let indexTestName = []
   let indexTestNameClose = []
-  let indexFileName = []
   let testResult = []
 
-
-  let countTestName = arrString.filter((item, i) => {
+ arrString.forEach((item, i) => {
     if(item == "✓" || item == "✕"){
       indexTestName.push(i)
-      return item
     }
-  })
-  let countTestNameClose = arrString.filter((item, i) => {
     if(item == "ms)"){
       indexTestNameClose.push(i+1)
       return item
