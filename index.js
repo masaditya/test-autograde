ac6428b2493842474fe0e178b60c38f2bb69a991
masaditya <aeka489@gmail.com>
2021-03-21 21:35:17 +0700

    create grade data

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ try {
     console.log(`Minggu ke-${week}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
-    const {payload } = github.context.payload
+    const {payload } = github.context
     console.log(payload)
     fs.readFile('test.log','utf-8', (err, data)=> {
       if(data){
